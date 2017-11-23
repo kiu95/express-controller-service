e052e6c29162175fc8f39a1a8f34615deffc0b8e
Calvin Wong <calckw.kiu95@gmail.com>
2017-11-24 01:00:45 +0800

    rename files publish first ver.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 'use strict';
+
+module.exports = {
+	Controller: require('./lib/Controller'),
+	Service: require('./lib/Service'),
+	CRUDController: require('./lib/CRUDController'),
+	CRUDService: require('./lib/CRUDService')
+};
